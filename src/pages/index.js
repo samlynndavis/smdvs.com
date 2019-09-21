1ed335ba9cbe0bbf54755c6dff377bc16f69d159
Samuel Davis <samueldavis@Samuels-MacBook-Air.local>
2019-09-20 19:49:27 -0500

    Updates homepage hero text and header

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,15 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import Head from '../components/head'
-import Blog from '../pages/blog'
+
+import layoutStyles from '../components/layout.module.scss'
 
 const IndexPage = () => {
 
     return (
         <Layout>
             <Head title="Home" />
-            <h1>Hello.</h1>
-            <h2>I'm Sam, a full-stack developer living and working in Chicago, Illinois.</h2>
+            <h1 className={layoutStyles.hero}><span className={layoutStyles.bold}>Hi.</span> I'm a <span className={layoutStyles.title}>front-end engineer</span> and <span className={layoutStyles.title}>visual artist</span> born, raised, and currently based in Chicago, Illinois.</h1>
         </Layout>
     )
 }
