90e5a20c4864756345687c91e3fa2c50952edf24
Samuel Davis <hello@smdvs.com>
2020-09-08 14:49:16 -0500

    Removes 'writing' nav item and decreases font size

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import { Link, graphql, useStaticQuery } from 'gatsby'
 import headerStyles from './header.module.scss'
 
 const Header = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
       query {
         site {
           siteMetadata {
@@ -14,26 +14,26 @@ const Header = () => {
         }
       }
     `)
-    return (
-      <header className={headerStyles.header}>
-        <nav>
-          <ul className={headerStyles.navList}>
-            <li>
-              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Writing</Link>
-            </li>
-            <li className={headerStyles.name}>
-              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">{data.site.siteMetadata.title}</Link>
-            </li>
-            <li className={headerStyles.nameMobile}>
-              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">SLD</Link>
-            </li>
-            <li>
-              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/projects">Projects</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    )
+  return (
+    <header className={headerStyles.header}>
+      <nav>
+        <ul className={headerStyles.navList}>
+          {/* <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Writing</Link>
+          </li> */}
+          <li className={headerStyles.name}>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">{data.site.siteMetadata.title}</Link>
+          </li>
+          <li className={headerStyles.nameMobile}>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">SLD</Link>
+          </li>
+          <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/projects">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
